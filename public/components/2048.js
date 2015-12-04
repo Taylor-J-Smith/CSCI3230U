0e6bf5639c90a928a5a1c844fa5c4a163983ed4b
Dennis Pacewicz <dennis.pacewicz@gmail.com>
2015-12-03 23:05:30 -0500

    added 2048 component

---
+++ public/components/2048.js
@@ -0,0 +1,417 @@
+module.exports = {
+  template: '<h1>{{page}}</h1><button v-on:click="new">reset game</button> <div id="tfe"><div id = "header"><div id = "scores"> <div id = "current-score"> <p>Current:</p> <p id = "score">0</p> </div> <div id = "best-score"> <p>Best:</p> <p>0</p> </div> </div> </div> <div id = "pre-game"> <p id = "info">Combine like tiles to gain points!</p> <div id = "new-game" onclick="newGame()">NEW GAME</div> </div> <div id = "game"> <svg></svg> </div> <div id = "post-game"> <div id = "rules"> <p> How to play: Use your arrow keys to move the tiles. When two tiles with the same number touch, they merge into one! </p> </div> <hr> <div id = "credits"> This game brought to you by: Time-Kill Games </div> </div> </div>',
+  replace: true,
+  data: function () {
+    return {
+      page: '2048'
+    }
+  },
+  methods:{
+    score: function(points){
+      if(window.localStorage['LOCAL_TOKEN_KEY'] != null){
+        Vue.http.headers.common['x-access-token'] = window.localStorage['LOCAL_TOKEN_KEY'];
+        var data = JSON.stringify({score: points})
+        this.$http.post('/api/score', data, function (data, status, request) {
+
+        }).error(function (data, status, request) {
+            // handle error
+            console.log("error")
+        })
+      }
+    },
+    game: function(){
+
+    },
+    new: function(){
+      
+      
+    }
+  },
+  ready: function(){
+    this.score(10000)
+    const UP = 38;
+    const DOWN = 40;
+    const LEFT = 37 ;
+    const RIGHT = 39;
+
+    const SVG_BACKGROUND_COLOUR = "#666666";
+    const DEFAULT_CURVE = 15;
+    const BACKGROUND_TILE_COLOUR = "#cccccc";
+
+    var rows = 4;
+    var columns = 4;
+
+    var board = create2DArray(rows, columns);
+    var joined = create2DArray(rows, columns);
+
+    var tileHeight = 100;
+    var tileWidth = 100;
+
+    var tileMargin = 10;
+
+    var score = 0;
+
+    d3.select("body")
+      .on("keydown", function()
+        {
+          d3.event.preventDefault();
+          keyHandler();
+        })
+
+    var svg = d3.select("svg")
+      .attr("height", 4*tileHeight + 5*tileMargin)
+      .attr("width", 4*tileWidth + 5*tileMargin);
+
+    var bGround = svg.append("rect")
+      .attr("height", 4*tileHeight + 5*tileMargin)
+      .attr("width", 4*tileWidth + 5*tileMargin)
+      .attr("rx", DEFAULT_CURVE)
+      .attr("ry", DEFAULT_CURVE)
+      .attr("fill", SVG_BACKGROUND_COLOUR);
+
+    var bGroundGroups = svg.selectAll("g")
+      .data(board)
+      .enter().append("g")
+        .selectAll("g")
+        .data(function(d){return d;})
+          .enter().append("g")
+            .attr("transform", function(d,i,j) {return "translate(" + ((j+1)*tileMargin + j*tileWidth) + "," + ((i+1)*tileMargin + i*tileHeight) + ")";})
+            .attr("id", function(d,i,j){return "G" + j + "-" + i;})
+
+    var bGroundTiles = bGroundGroups.append("rect")
+      .attr("height", tileHeight)
+      .attr("width", tileWidth)
+      .attr("fill", BACKGROUND_TILE_COLOUR)
+      .attr("rx", DEFAULT_CURVE)
+      .attr("ry", DEFAULT_CURVE);
+
+    var numbers = bGroundGroups.append("text")
+      .attr("x", tileWidth/2)
+      .attr("y", tileHeight/2)
+      .attr("font-size", 50)
+      .attr("alignment-baseline", "middle")
+      .attr("text-anchor", "middle")
+
+    init(board);
+
+    function init(board)
+    {
+      var score = 0;
+      updateScore();
+
+      var locations = generateRandomNumbers(2, 0, 15);
+
+      for(var i = 0; i < locations.length; i++)
+      {
+        var j = Math.floor(locations[i] / columns);
+        var k = locations[i] % columns;
+
+        board[j][k] = 2;
+      }
+
+      updateTiles();
+    }
+
+    function generateRandomNumbers(n, min, max)
+    {
+      if(n > max - min)
+      {
+        console.error("Asking for more numbers than in range.");
+        return;
+      }
+
+      var randNumbers = [];
+      var count = 0;
+
+      while(count < n)
+      {
+        var r = Math.floor(Math.random() * max) + min;
+
+        if(randNumbers.indexOf(r) == -1)
+        {
+          randNumbers.push(r);
+          count++;
+        }
+      }
+
+      return randNumbers;
+    }
+
+    function print2DArray(array)
+    {
+        for(var i = 0; i < array.length; i++)
+        {
+          console.log(array[i]);
+        }
+    }
+
+    function create2DArray(rows, columns)
+    {
+      var Array2D = [];
+
+      for(var i = 0; i < rows; i++)
+      {
+        Array2D[i] = new Array(columns);
+
+        for(var j = 0; j < columns; j++)
+        {
+          Array2D[i][j] = 0;
+        }
+      }
+
+      return Array2D;
+    }
+
+    function set2DArray(array, value)
+    {
+      for(var i = 0; i < array.length; i++)
+      {
+        for(var j = 0; j < array[0].length; j++)
+        {
+          array[i][j] = value;
+        }
+      }
+    }
+
+    function keyHandler()
+    {
+      var keyCode = d3.event.keyCode;
+
+      if(keyCode == UP)
+      {
+        up();
+      }
+      if(keyCode == DOWN)
+      {
+        down();
+      }
+      if(keyCode == LEFT)
+      {
+        left();
+      }
+      if(keyCode == RIGHT)
+      {
+        right();
+      }
+      updateTiles();
+      updateScore();
+    }
+
+    function up()
+    {
+      set2DArray(joined, 0);
+      var numMoved = 0;
+
+      for(var i = 0; i < columns; i++)
+      {
+        for(var j = 1; j < rows; j++)
+        {
+          var k=j;
+          while(k > 0)
+          {
+            if(board[j-k][i] == 0 && board[j][i] != 0)
+            {
+              board[j-k][i] = board[j][i];
+              board[j][i] = 0;
+              numMoved++
+            }
+
+            if(board[j-k][i] == board[j][i] && !joined[j-k][i] && board[j][i] !=0)
+            {
+              board[j-k][i] = 2*board[j][i];
+              score += 2*board[j][i];
+              board[j][i] = 0;
+              joined[j-k][i] = 1;
+              numMoved++;
+            }
+            k--;
+          }
+        }
+      }
+
+      if(numMoved > 0)
+      {
+        spawnNewTile();
+      }
+
+      print2DArray(board);
+    }
+
+    function down()
+    {
+      set2DArray(joined, 0);
+      var numMoved = 0;
+
+      for(var i = 0; i < columns; i++)
+      {
+        for(var j = rows - 2; j >= 0; j--)
+        {
+          var k=0;
+          while(j+k < rows-1)
+          {
+            if(board[j+k+1][i] == 0 && board[j + k][i] != 0)
+            {
+              board[j+k+1][i] = board[j + k][i];
+              board[j+k][i] = 0;
+              numMoved++
+            }
+
+            if(board[j+k+1][i] == board[j+k][i] && !joined[j+k+1][i] && board[j+k][i] !=0)
+            {
+              board[j+k+1][i] = board[j+k][i]+board[j+k+1][i];
+              score += 2*board[j+k][i];
+              board[j+k][i] = 0;
+              joined[j+k+1][i] = 1;
+              numMoved++;
+            }
+            k++;
+          }
+        }
+      }
+
+      if(numMoved > 0)
+      {
+        spawnNewTile();
+      }
+
+      print2DArray(board);
+    }
+
+    function left()
+    {
+      set2DArray(joined, 0);
+      var numMoved = 0;
+
+      for(var i = 0; i < columns; i++)
+      {
+        for(var j = 1; j < rows; j++)
+        {
+          var k=j;
+          while(k > 0)
+          {
+            if(board[i][j-k] == 0 && board[i][j] != 0)
+            {
+              board[i][j-k] = board[i][j];
+              board[i][j] = 0;
+              numMoved++
+            }
+
+            if(board[i][j-k] == board[i][j] && !joined[i][j-k] && board[i][j] !=0)
+            {
+              board[i][j-k] = 2*board[i][j];
+              score += 2*board[i][j];
+              board[i][j] = 0;
+              joined[i][j-k] = 1;
+              numMoved++;
+            }
+            k--;
+          }
+        }
+      }
+
+      if(numMoved > 0)
+      {
+        spawnNewTile();
+      }
+
+      print2DArray(board);
+    }
+
+    function right()
+    {
+      set2DArray(joined, 0);
+      var numMoved = 0;
+
+      for(var i = 0; i < columns; i++)
+      {
+        for(var j = rows - 2; j >= 0; j--)
+        {
+          var k=0;
+          while(j+k < rows-1)
+          {
+            if(board[i][j+k+1] == 0 && board[i][j + k] != 0)
+            {
+              board[i][j+k+1] = board[i][j + k];
+              board[i][j+k] = 0;
+              numMoved++
+            }
+
+            if(board[i][j+k+1] == board[i][j+k] && !joined[i][j+k+1] && board[i][j+k] !=0)
+            {
+              board[i][j+k+1] = board[i][j+k]+board[i][j+k+1];
+              score += 2*board[i][j+k];
+              board[i][j+k] = 0;
+              joined[i][j+k+1] = 1;
+              numMoved++;
+            }
+            k++;
+          }
+        }
+      }
+
+      if(numMoved > 0)
+      {
+        spawnNewTile();
+      }
+
+      print2DArray(board);
+    }
+
+    function newGame()
+    {
+      set2DArray(board, 0);
+      init(board);
+    }
+
+    function spawnNewTile()
+    {
+      newTile = false;
+
+      while(!newTile)
+      {
+        var r = generateRandomNumbers(1,0,15);
+
+        var j = Math.floor(r / columns);
+        var k = r % columns;
+
+        if(board[j][k] == 0)
+        {
+          board[j][k] = 2;
+          newTile = true;
+        }
+      }
+
+    }
+
+    function updateTiles()
+    {
+      for(var i = 0; i < rows; i++)
+      {
+        for(var j = 0; j < columns; j++)
+        {
+          if(board[i][j] != 0)
+          {
+            d3.select("#G" + j + "-" + i)
+              .attr("class", "N" + board[i][j])
+              .select("text")
+              .text(board[i][j])
+          }
+          else
+          {
+            d3.select("#G" + j + "-" + i)
+              .attr("class", "N0")
+              .select("text")
+              .text(null)
+          }
+
+        }
+      }
+    }
+
+    function updateScore()
+    {
+      d3.select("#score")
+        .text(score)
+    }
+  }
+}
